d8ff420380aeaf004512d37f0badcd83f40e2f2f
Lee Kaburu <leekaburu@gmail.com>
2025-02-02 12:35:54 +0300

    Updates and corrections

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,12 @@
-USE alx_book_store;
-
-SHOW CREATE TABLE Books;
+SELECT
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
